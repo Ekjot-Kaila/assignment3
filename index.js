b56a6288efd24c03ec52364eb7381f8db84b3972
“EkjotKaila” <ekaila1@my.bcit.ca>
2023-05-15 01:41:23 -0700

    pagination

--- index.js
+++ index.js
@@ -1,104 +1,156 @@
-const PAGE_SIZE = 10
+const MAX_PAGES = 5;
 let currentPage = 1;
-let pokemons = []
-
+let pokemons = [];
 
 const updatePaginationDiv = (currentPage, numPages) => {
-  $('#pagination').empty()
+  $('#pagination').empty();
 
-  const startPage = 1;
-  const endPage = numPages;
-  for (let i = startPage; i <= endPage; i++) {
-    $('#pagination').append(`
-    <button class="btn btn-primary page ml-1 numberedButtons" value="${i}">${i}</button>
-    `)
+  let startPage = Math.max(currentPage - 2, 1);
+  let endPage = Math.min(startPage + 4, numPages);
+
+  if (endPage - startPage < 4) {
+    startPage = Math.max(endPage - 4, 1);
   }
 
-}
+  for (let i = startPage; i <= endPage; i++) {
+    let buttonClass = i === currentPage ? 'btn-primary active' : 'btn-primary';
+    $('#pagination').append(`
+      <button class="btn ${buttonClass} page ml-1 numberedButtons" value="${i}">${i}</button>
+    `);
+  }
 
-const paginate = async (currentPage, PAGE_SIZE, pokemons) => {
-  selected_pokemons = pokemons.slice((currentPage - 1) * PAGE_SIZE, currentPage * PAGE_SIZE)
+  // Add Next and Previous buttons
+  $('#pagination').prepend(`
+    <button class="btn btn-primary ml-1" id="prevButton">Previous</button>
+  `);
 
-  $('#pokeCards').empty()
-  selected_pokemons.forEach(async (pokemon) => {
-    const res = await axios.get(pokemon.url)
+  $('#pagination').append(`
+    <button class="btn btn-primary ml-1" id="nextButton">Next</button>
+  `);
+
+  // Disable Next and Previous buttons if on first or last page
+  if (currentPage === 1) {
+    $('#prevButton').prop('disabled', true);
+  } else if (currentPage === numPages) {
+    $('#nextButton').prop('disabled', true);
+  }
+};
+
+
+const paginate = async () => {
+  const pageSize = 10;
+  const selectedPokemons = pokemons.slice(
+    (currentPage - 1) * pageSize,
+    currentPage * pageSize
+  );
+
+  $('#pokeCards').empty();
+  for (const pokemon of selectedPokemons) {
+    const res = await axios.get(pokemon.url);
     $('#pokeCards').append(`
-      <div class="pokeCard card" pokeName=${res.data.name}   >
+      <div class="pokeCard card" pokeName=${res.data.name}>
         <h3>${res.data.name.toUpperCase()}</h3> 
         <img src="${res.data.sprites.front_default}" alt="${res.data.name}"/>
         <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#pokeModal">
           More
         </button>
-        </div>  
-        `)
-  })
-}
+      </div>  
+    `);
+  }
+};
+
+const displayPokemonCount = (total, displayed) => {
+  $('#pokemonCount').html(`Total Pokémon: ${total} | Displayed: ${displayed}`);
+};
 
 const setup = async () => {
-  // test out poke api using axios here
+  $('#pokeCards').empty();
 
-
-  $('#pokeCards').empty()
-  let response = await axios.get('https://pokeapi.co/api/v2/pokemon?offset=0&limit=810');
+  const response = await axios.get(
+    'https://pokeapi.co/api/v2/pokemon?offset=0&limit=810'
+  );
   pokemons = response.data.results;
 
+  const numPages = Math.ceil(pokemons.length / 10);
+  paginate();
+  updatePaginationDiv(currentPage, numPages);
+  displayPokemonCount(pokemons.length, 10);
 
-  paginate(currentPage, PAGE_SIZE, pokemons)
-  const numPages = Math.ceil(pokemons.length / PAGE_SIZE)
-  updatePaginationDiv(currentPage, numPages)
-
-
-
-  // pop up modal when clicking on a pokemon card
-  // add event listener to each pokemon card
   $('body').on('click', '.pokeCard', async function (e) {
-    const pokemonName = $(this).attr('pokeName')
-    // console.log("pokemonName: ", pokemonName);
-    const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
-    // console.log("res.data: ", res.data);
-    const types = res.data.types.map((type) => type.type.name)
-    // console.log("types: ", types);
+    const pokemonName = $(this).attr('pokeName');
+    const res = await axios.get(
+      `https://pokeapi.co/api/v2/pokemon/${pokemonName}`
+    );
+    const types = res.data.types.map((type) => type.type.name);
+
     $('.modal-body').html(`
-        <div style="width:200px">
+      <div style="width:200px">
         <img src="${res.data.sprites.other['official-artwork'].front_default}" alt="${res.data.name}"/>
-        <div>
-        <h3>Abilities</h3>
-        <ul>
-        ${res.data.abilities.map((ability) => `<li>${ability.ability.name}</li>`).join('')}
-        </ul>
-        </div>
-
-        <div>
-        <h3>Stats</h3>
-        <ul>
-        ${res.data.stats.map((stat) => `<li>${stat.stat.name}: ${stat.base_stat}</li>`).join('')}
-        </ul>
-
-        </div>
-
-        </div>
-          <h3>Types</h3>
-          <ul>
-          ${types.map((type) => `<li>${type}</li>`).join('')}
-          </ul>
-      
-        `)
-    $('.modal-title').html(`
-        <h2>${res.data.name.toUpperCase()}</h2>
-        <h5>${res.data.id}</h5>
-        `)
-  })
-
-  // add event listener to pagination buttons
-  $('body').on('click', ".numberedButtons", async function (e) {
-    currentPage = Number(e.target.value)
-    paginate(currentPage, PAGE_SIZE, pokemons)
-
-    //update pagination buttons
-    updatePaginationDiv(currentPage, numPages)
-  })
-
-}
-
-
-$(document).ready(setup)
+                <div>
+                  <h3>Abilities</h3>
+                  <ul>
+                    ${res.data.abilities
+                      .map((ability) => `<li>${ability.ability.name}</li>`)
+                      .join('')}
+                  </ul>
+                </div>
+        
+                <div>
+                  <h3>Stats</h3>
+                  <ul>
+                    ${res.data.stats
+                      .map((stat) => `<li>${stat.stat.name}: ${stat.base_stat}</li>`)
+                      .join('')}
+                  </ul>
+                </div>
+              </div>
+        
+              <h3>Types</h3>
+              <ul>
+                ${types.map((type) => `<li>${type}</li>`).join('')}
+              </ul>
+            `);
+        
+            $('.modal-title').html(`
+            <h2>${res.data.name.toUpperCase()}</h2>
+            <h5>${res.data.id}</h5>
+          `);
+        });
+        
+        $('body').on('click', '.numberedButtons', async function (e) {
+          currentPage = Number(e.target.value);
+          paginate();
+          updatePaginationDiv(currentPage, numPages);
+        });
+        
+        // Next button event listener
+        $('body').on('click', '#nextButton', function () {
+          if (currentPage < numPages) {
+            currentPage++;
+            paginate();
+            updatePaginationDiv(currentPage, numPages);
+            $('#prevButton').show(); // Show Previous button
+          }
+        
+          if (currentPage === numPages) {
+            $('#nextButton').hide(); // Hide Next button
+          }
+        });
+        
+        // Previous button event listener
+        $('body').on('click', '#prevButton', function () {
+          if (currentPage > 1) {
+            currentPage--;
+            paginate();
+            updatePaginationDiv(currentPage, numPages);
+            $('#nextButton').show(); // Show Next button
+          }
+        
+          if (currentPage === 1) {
+            $('#prevButton').hide(); // Hide Previous button
+          }
+        });
+        
+        };
+        
+        $(document).ready(setup);
